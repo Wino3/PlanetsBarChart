633b0bc81cf6062586325720c958e84d2dfe7654
Cher Hill <cherkhill@gmail.com>
2019-11-28 23:07:55 -0800

    Add project files.

---
+++ Planets Bar Chart/frmPlanets.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPlanets
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstPlanets = New System.Windows.Forms.ListBox()
+        Me.btnShow = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.lblPrgmr = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lstPlanets
+        '
+        Me.lstPlanets.FormattingEnabled = True
+        Me.lstPlanets.Location = New System.Drawing.Point(173, 106)
+        Me.lstPlanets.Name = "lstPlanets"
+        Me.lstPlanets.Size = New System.Drawing.Size(411, 238)
+        Me.lstPlanets.TabIndex = 0
+        '
+        'btnShow
+        '
+        Me.btnShow.Location = New System.Drawing.Point(342, 53)
+        Me.btnShow.Name = "btnShow"
+        Me.btnShow.Size = New System.Drawing.Size(75, 23)
+        Me.btnShow.TabIndex = 1
+        Me.btnShow.Text = "Show"
+        Me.btnShow.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(214, 373)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(492, 373)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'lblPrgmr
+        '
+        Me.lblPrgmr.AutoSize = True
+        Me.lblPrgmr.Location = New System.Drawing.Point(12, 9)
+        Me.lblPrgmr.Name = "lblPrgmr"
+        Me.lblPrgmr.Size = New System.Drawing.Size(46, 26)
+        Me.lblPrgmr.TabIndex = 4
+        Me.lblPrgmr.Text = "Cher Hill" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "CS115"
+        '
+        'frmPlanets
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblPrgmr)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnShow)
+        Me.Controls.Add(Me.lstPlanets)
+        Me.Name = "frmPlanets"
+        Me.Text = "Planets Bar Chart"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstPlanets As ListBox
+    Friend WithEvents btnShow As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents lblPrgmr As Label
+End Class
---
+++ Planets Bar Chart/frmPlanets.vb
@@ -0,0 +1,17 @@
+﻿
+Option Strict On
+Option Explicit On
+Public Class frmPlanets
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        'Number of planets before Pluto was "let go"
+        Const intNUM_PLANETS As Integer = 9
+        'Planet names
+        Dim strArrayPlanetNames() As String =
+        {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"}
+        'diameter of each planet in miles
+        Dim intArrayPlanetSizes() As Integer =
+        {3031, 7521, 7926, 4223, 88846, 74898, 31763, 30800, 1430}
+
+
+    End Sub
+End Class
