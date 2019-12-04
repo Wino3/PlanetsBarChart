bd0863754b07523983a77e5f55f0f21e662c97ac
Cher Hill <Cher Hill@DESKTOP-E086QTC>
2019-12-04 15:09:29 -0800

    Planets

--- Planets Bar Chart/frmPlanets.vb
+++ Planets Bar Chart/frmPlanets.vb
@@ -3,6 +3,7 @@ Option Strict On
 Option Explicit On
 Public Class frmPlanets
     Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+
         'Number of planets before Pluto was "let go"
         Const intNUM_PLANETS As Integer = 9
         'Planet names
@@ -12,9 +13,24 @@ Public Class frmPlanets
         Dim intArrayPlanetSizes() As Integer =
         {3031, 7521, 7926, 4223, 88846, 74898, 31763, 30800, 1430}
 
+        For P As Integer = 0 To intNUM_PLANETS - 1
+            Dim strPlanets As String
+            strPlanets = $"Planets: {strArrayPlanetNames(P)} Diameter:{intArrayPlanetSizes(P)}"
+            lstPlanets.Items.Add(strPlanets)
+
+            GetDiameterAsAsterisks(intArrayPlanetSizes(P))
+        Next
 
     End Sub
+    Function GetDiameterAsAsterisks(intDiameter As Integer) As String
+        Dim intNumAsterisks As Integer = intDiameter \ 1000
+        Dim strResults As String = ""
+        For A As Integer = 1 To intNumAsterisks
+            strResults = "*" & strResults
+        Next
+        Return strResults
 
+    End Function
     Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
         lstPlanets.ClearSelected()
     End Sub
